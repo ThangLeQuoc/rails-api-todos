4be32cf3f66c025c480d5607e79dee6b612041bb
Thang Le Quoc <thanglequoc.it@gmail.com>
2020-03-09 22:18:30 +0700

    Add Test Suite and Controller Methods for Items

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,6 +13,7 @@ class TodosController < ApplicationController
     # Answer: In our create method in the TodosController, note that we're using create! instead of create. This way, the model will raise an exception ActiveRecord::RecordInvalid. This way, we can avoid deep nested if statements in the controller. Thus, we rescue from this exception in the ExceptionHandler module.
     @todo = Todo.create!(todo_params)
     # TODO @thangle: what initialize this created variable ?
+    # Answer: created is the symbol of the http status
     json_response(@todo, :created)
   end
 
